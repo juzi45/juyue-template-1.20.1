41dfc64bdd87fab3bf6d345c32f79308889b004b
juzi45 <juzidroid@outlook.com>
2023-08-31 08:15:58 +0800

    alpha 0.0.1

---
+++ src/main/java/name/juyue/juyueClient.java
@@ -0,0 +1,10 @@
+package name.juyue;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class juyueClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/name/juyue/juyueDataGenerator.java
@@ -0,0 +1,11 @@
+package name.juyue;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class juyueDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
