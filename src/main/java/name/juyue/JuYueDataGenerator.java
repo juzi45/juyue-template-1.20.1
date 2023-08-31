ba005465426938237c8aa04d305dca814ef95ad3
KrLite <68179735+KrLite@users.noreply.github.com>
2023-08-31 16:07:33 +0800

    Codestyle

--- src/main/java/name/juyue/JuYueDataGenerator.java
+++ src/main/java/name/juyue/JuYueDataGenerator.java
@@ -3,9 +3,8 @@ package name.juyue;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 
-public class juyueDataGenerator implements DataGeneratorEntrypoint {
+public class JuYueDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
-
 	}
 }
--- src/main/java/name/juyue/register/items.java
+++
@@ -1,5 +0,0 @@
-package name.juyue.register;
-
-public class items {
-    //public static final Item xuan =juyue_items_registers(namespace: )
-}
