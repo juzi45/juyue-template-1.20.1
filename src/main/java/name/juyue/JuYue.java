0fc61667f98a0f1327fb360d529eae8e248a0448
juzi45 <juzidroid@outlook.com>
2023-09-01 21:23:12 +0800

    0.0.1 update for Items load

--- src/main/java/name/juyue/JuYue.java
+++ src/main/java/name/juyue/JuYue.java
@@ -1,5 +1,6 @@
 package name.juyue;
 
+import name.juyue.content.JuYueItems;
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,5 +11,6 @@ public class JuYue implements ModInitializer {
 
     @Override
     public void onInitialize() {
+        JuYueItems.RegisterItem();
     }
 }
--- src/main/java/name/juyue/content/JuYueItems.java
+++ src/main/java/name/juyue/content/JuYueItems.java
@@ -7,10 +7,13 @@ import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 
+
 public class JuYueItems {
 	public static final Item XUAN = RegisterItem(JuYue.MOD_ID, "xuan");
 
 	public static Item RegisterItem(String namespace, String id) {
 		return Registry.register(Registries.ITEM, new Identifier(namespace, id), new Item(new FabricItemSettings()));
 	}
-}
+	public static void RegisterItem(){
+		JuYue.LOGGER.debug("Loading for Items of"+JuYue.MOD_ID,"Mod");
+}}
