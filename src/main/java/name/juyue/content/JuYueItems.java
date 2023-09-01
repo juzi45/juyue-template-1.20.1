cb3187d27163012ca2521da73f2b01404526f286
juzi45 <juzidroid@outlook.com>
2023-09-01 22:16:21 +0800

    0.0.1 update for Items load of new a test Item

--- src/main/java/name/juyue/content/JuYueItems.java
+++ src/main/java/name/juyue/content/JuYueItems.java
@@ -9,10 +9,9 @@ import net.minecraft.util.Identifier;
 
 
 public class JuYueItems {
-	public static final String Item="item"; String Tool="tools";
 
-	public static final Item XUAN = RegisterItem(Item, "xuan");
-	public static final Item JINGSHI = RegisterItem(Item, "baboljinsi");
+	public static final Item XUAN = RegisterItem(JuYue.MOD_ID, "xuan");
+	public static final Item JINGSHI = RegisterItem(JuYue.MOD_ID, "baboljinsi");
 	public static Item RegisterItem(String namespace, String id) {
 		return Registry.register(Registries.ITEM, new Identifier(namespace, id), new Item(new FabricItemSettings()));
 	}
