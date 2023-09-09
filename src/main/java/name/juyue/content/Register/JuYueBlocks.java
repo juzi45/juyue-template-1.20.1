5ac8407e18f43545df28f832358eb67e3f22f605
juzi45 <juzidroid@outlook.com>
2023-09-09 15:23:33 +0800

    auto paste [自动提交]

--- src/main/java/name/juyue/content/Register/JuYueBlocks.java
+++ src/main/java/name/juyue/content/Register/JuYueBlocks.java
@@ -1,7 +1,9 @@
 package name.juyue.content.Register;
 
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
+import net.minecraft.item.BlockItem;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
@@ -11,6 +13,5 @@ public class JuYueBlocks {
 
     public static Block RegisterBlocks(String id,String b){
         return Registry.register(Registries.BLOCK,new Identifier(JuYue.MOD_ID,id),new Block(new FabricBlockSettings()));
-
     }
 }
